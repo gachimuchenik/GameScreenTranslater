00f9de896df60a5a0a4bf2d65236801e80515f05
gachimuchenik <gachimuchenik3228@gmail.com>
2023-08-26 17:49:54 +0400

    ver 1.1 optimization

--- AutoTranslater.py
+++ AutoTranslater.py
@@ -20,8 +20,6 @@ sharpening_kernel = np.array([
 preset = 1
 last_result = None
 last_phone = None
-x1, y1, x2, y2 = (450, 765, 1480, 1000)
-px1, py1, px2, py2 = (450, 765, 1480, 1000)
 
 
 def tr(image):
@@ -79,10 +77,10 @@ def tr_phone(image, image_phone):
             pass
 
 
-def tr_cut(cut):
+def tr_cut_mess(image):
     global last_result
     pytesseract.pytesseract.tesseract_cmd = r"D:\Tesseract\tesseract.exe"
-    result = pytesseract.image_to_string(cut, config=custom_conf, output_type='string')
+    result = pytesseract.image_to_string(image, config=custom_conf, output_type='string')
     if result != last_result:
         try:
             text = re.sub("\n", " ", result)
@@ -100,87 +98,58 @@ def tr_cut(cut):
 def crop(image):
     match preset:
         case 1:
-            image = image.crop((450, 765, 1480, 1000))
-            image.save('subs.png', 'png')
-            preprocessing()
+            crop_sub = image[765:1000, 450:1480]
+            preprocessing(crop_sub)
         case 2:
-            image_phone = image
-            image_phone = image_phone.crop((500, 100, 1500, 260))
-            image_phone.save('phone.png', 'png')
-            image = image.crop((450, 765, 1480, 1000))
-            image.save('subs.png', 'png')
-            preprocessing_phone()
+            crop_phone = image[100:260, 500:1500]
+            crop_sub = image[765:1000, 450:1480]
+            preprocessing_phone(crop_sub, crop_phone)
         case 3:
-            image = image.crop((440, 880, 1480, 1050))
-            image.save('cut.png', 'png')
-            preprocessing_cutscene()
+            crop_cut = image[880:1050, 440:1480]
+            preprocessing_cutscene(crop_cut)
+        case 4:
+            mess_crop = image[400:875, 630:1320]
+            preprocessing_message(mess_crop)
 
 
-def preprocessing():
-    image = cv2.imread('subs.png')
+def preprocessing(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, image = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     image = cv2.medianBlur(image, 3)
     image = cv2.filter2D(image, -1, sharpening_kernel)
-    # cv2.imwrite('image.png', image)
     tr(image)
 
 
-def preprocessing_phone():
-    image = cv2.imread('subs.png')
+def preprocessing_phone(image, image_phone):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, image = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     image = cv2.medianBlur(image, 3)
     image = cv2.filter2D(image, -1, sharpening_kernel)
-    image_phone = cv2.imread('phone.png')
     gray = cv2.cvtColor(image_phone, cv2.COLOR_BGR2GRAY)
     _, image_phone = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     image_phone = cv2.medianBlur(image_phone, 3)
     image_phone = cv2.filter2D(image_phone, -1, sharpening_kernel)
-    # cv2.imwrite('image.png', image)
-    # cv2.imwrite('imagephone.png', image_phone)
     tr_phone(image, image_phone)
 
 
-def preprocessing_cutscene():
-    image = cv2.imread('cut.png')
+def preprocessing_cutscene(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     lower_white = np.array([230, 230, 230])
     upper_white = np.array([255, 255, 255])
     mask = cv2.inRange(image, lower_white, upper_white)
-    result = cv2.bitwise_and(image, image, mask=mask)
-    result[np.where((result == [0, 0, 0]).all(axis=2))] = [0, 0, 0]
-    result = cv2.bitwise_not(result)
-    result = cv2.medianBlur(result, 3)
-    result = cv2.filter2D(result, -1, sharpening_kernel)
-    # cv2.imwrite('test.png', result)
-    tr_cut(result)
+    image = cv2.bitwise_and(image, image, mask=mask)
+    image[np.where((image == [0, 0, 0]).all(axis=2))] = [0, 0, 0]
+    image = cv2.bitwise_not(image)
+    image = cv2.medianBlur(image, 3)
+    image = cv2.filter2D(image, -1, sharpening_kernel)
+    tr_cut_mess(image)
 
-
-# def switch_case_dialog():
-#     global preset
-#     preset = 1
-#     print(Fore.YELLOW + 'preset - dialog')
-#
-#
-# def switch_case_phone_dialog():
-#     global preset
-#     preset = 2
-#     print(Fore.YELLOW + 'preset - phone dialog')
-#
-#
-# def switch_case_cutscene():
-#     global preset
-#     preset = 3
-#     print(Fore.YELLOW + 'preset - cutscene')
-#
-#
-# dialog_key = 'z'
-# phone_dialog_key = 'x'
-# cutscene_key = 'c'
-# keyboard.add_hotkey(dialog_key, switch_case_dialog)
-# keyboard.add_hotkey(phone_dialog_key, switch_case_phone_dialog)
-# keyboard.add_hotkey(cutscene_key, switch_case_cutscene)
+def preprocessing_message(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, image = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    image = cv2.medianBlur(image, 3)
+    image = cv2.filter2D(image, -1, sharpening_kernel)
+    tr_cut_mess(image)
 
 
 def main():
@@ -191,6 +160,7 @@ def main():
         if block_number == 'auto':
             while True:
                 screen = pyautogui.screenshot()
+                screen = np.array(screen)
                 crop(screen)
                 sleep(0.5)
                 if keyboard.is_pressed('f'):
@@ -204,11 +174,15 @@ def main():
                 if keyboard.is_pressed('c'):
                     preset = 3
                     print(Fore.YELLOW + 'preset - cutscene')
+                if keyboard.is_pressed('v'):
+                    preset = 4
+                    print(Fore.YELLOW + 'preset - message')
         elif block_number == 'screen':
             while True:
                 clipboard_image = ImageGrab.grabclipboard()
                 if clipboard_image is not None and clipboard_image != last_clipboard_image:
-                    crop(clipboard_image)
+                    screen = np.array(clipboard_image)
+                    crop(screen)
                     last_clipboard_image = clipboard_image
                 sleep(0.5)
                 if keyboard.is_pressed('f'):
@@ -222,6 +196,9 @@ def main():
                 if keyboard.is_pressed('c'):
                     preset = 3
                     print(Fore.YELLOW + 'preset - cutscene')
+                if keyboard.is_pressed('v'):
+                    preset = 4
+                    print(Fore.YELLOW + 'preset - message')
         block_number = 'auto' if block_number == 'screen' else 'screen'
         print(Fore.YELLOW + block_number)
 
